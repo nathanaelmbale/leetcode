210209126e3b701ef466b52d2ed418424fce965f
Nathanael Mbale <66083904+nathanaelmbale@users.noreply.github.com>
2024-07-05 15:11:37 -0400

    failure

---
+++ 43-trapping-rain-water.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(ratings) {
+    let candies = 0 
+    let l = 0
+    let value = []
+
+    
+    
+    while (l < ratings.length) {
+        
+    }
+
+
+};
